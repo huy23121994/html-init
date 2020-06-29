e5acf49e107435dc3142b1247e6e0c0948a441b3
Duy tạ <duyta291297@gmail.com>
2020-06-29 08:38:21 +0700

    feat: update UI course page & tour page

--- docs/js/app.js
+++ docs/js/app.js
@@ -15,6 +15,82 @@ $(document).ready(function () {
   $('.features-item').click(function() {
     showImg(this)
   })
+
+  // handle active combobox
+  $('[data-type="combobox"]').click(function() {
+    $(this).toggleClass('active');
+  })
+
+  // handle click out side combobox
+  var comboboxs = $('[data-type="combobox"]');
+  $(this).on('click', function ($event) {
+    comboboxs.toArray().forEach(function (combobox) {
+      if (
+        !$(combobox).is($event.target) &&
+        $(combobox).has($event.target).length === 0) {
+        $(combobox).removeClass('active');
+      }
+    })
+  })
+
+  // handle range-slider changes value to update UI
+  $('[data-type="rangeSlider"]').toArray().forEach(function(rangeSlider) {
+    // progress element
+    var rangeSliderProgress = $(rangeSlider).children('[data-type="rangeSliderProgress"]');
+    
+    // input element
+    var rangeInput = $(rangeSlider).children('input[type="range"]');
+
+    // update width rangeSliderProgress when input change 
+    rangeInput.on('input', function ($event) {
+      updateRangeSliderProgressData($event, rangeSliderProgress, rangeInput);
+    });
+
+    // init rangeSliderProgress width
+    var initWidthEvent = {
+      target: {
+        value: rangeInput.attr('value'),
+        attributes: {
+          max: {
+            value: rangeInput.attr('max')
+          },
+          min: {
+            value: rangeInput.attr('min')
+          }
+        }
+      }
+    }
+    updateRangeSliderProgressData(initWidthEvent, rangeSliderProgress, rangeInput);
+  })
+
+  // handle slideDown
+  $('[data-name="slideDown"]').on('click', function ($event) {
+    if ($(this).hasClass('active')) {
+      return $(this).removeClass('active');
+    }
+
+    var group = $(this).attr('data-group');
+    if (group) {
+      var itemsHaveSameGroup = $('[data-name="slideDown"][data-group="' + group + '"]');
+      itemsHaveSameGroup.toArray().forEach(function (item) {
+        $(item).removeClass('active');
+      })
+    }
+    $(this).addClass('active');
+  })
+
+  // handle tab
+  $('[data-name="tabNavItem"]').on('click', function () {
+    console.log($(this).attr('data-tab'));
+    var dataTab = $(this).attr('data-tab');
+
+    var tabEle = $(this).closest('[data-name="tab"]');
+    $(tabEle).find('[data-name="tabNavItem"]').removeClass('active');
+    $(tabEle).find('[data-name="tabContentItem"]').removeClass('active');
+
+    $(tabEle).find('[data-name="tabNavItem"][data-tab="' + dataTab + '"]').addClass('active');
+    $(tabEle).find('[data-name="tabContentItem"][data-tab="' + dataTab + '"]').addClass('active');
+  })
 });
 
 var makeSwipe = (function () {
@@ -45,3 +121,20 @@ var showImg = function (context) {
     })
   }
 };
+
+var updateRangeSliderProgressData = function ($event, rangeSliderProgress, rangeInput) {
+  // get the current value
+  var currentValue = $event.target.value;
+  var max = $event.target.attributes.max.value;
+  var min = $event.target.attributes.min.value;
+  var rangeInputWidth = rangeInput.width();
+  var circleWidth = 15;
+
+  
+  // var percent = Math.floor((currentValue - 30) / (max - min) * 10000) / 100 + '%';
+  var percent = (currentValue / (max - min) * (1 - circleWidth/rangeInputWidth)) * 100 + '%';
+
+  rangeSliderProgress
+    .css('width', percent)
+    .attr('data-value', currentValue);
+}
